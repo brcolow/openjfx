19a78f05f5f21c7898cdec9e56d49a1c8e697648
Michael Ennen <mike.ennen@gmail.com>
2017-12-08 01:56:02 -0700

    Improve gradle test logging, don't print crash logs by default.

--- .ci/script.sh
+++ .ci/script.sh
@@ -8,32 +8,30 @@ ulimit -c unlimited -S
 
 # Print core dumps when JVM crashes.
 RESULT=$?
-echo "Exit code from gradle build: $RESULT"
 
 if [[ ${RESULT} -ne 0 ]]; then
-  printf '\n\n======== JVM CRASH LOGS ======='
+  if [ "${PRINT_CRASH_LOGS}" = "true" ]; then
 
-  if [[ "${TRAVIS_OS_NAME}" == osx ]]; then FIND="gfind"; else FIND="find"; fi
-  ${FIND} . -name "hs_err_pid*.log" -type f -printf '\n%p\n' -exec cat {} \;
+    if [[ "${TRAVIS_OS_NAME}" == osx ]]; then FIND="gfind"; else FIND="find"; fi
+    ${FIND} . -name "hs_err_pid*.log" -type f -printf '\n====== JVM CRASH LOG ======\n%p\n' -exec cat {} \;
 
-  CORES=''
-  if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
-    CORES="$(find /cores/ -type f -print)"
-  else
-    CORES="$(find . -type f -regex '*core.[0-9]{6}' -print)"
-  fi
-
-  if [ -n "${CORES}" ]; then
-    for core in ${CORES}; do
-    printf '\n\n======= Core file %s =======' "$core"
+    CORES=''
     if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
-      lldb -Q -o "bt all" -f "$(which java)" -c "${core}"
+      CORES="$(find /cores/ -type f -print)"
     else
-      gdb -n -batch -ex "thread apply all bt" -ex "set pagination 0" "$(which java)" -c "${core}"
+      CORES="$(find . -type f -regex '*core.[0-9]{6}' -print)"
     fi
-  done
-  fi
 
+    if [ -n "${CORES}" ]; then
+      for core in ${CORES}; do
+      printf '\n\n======= Core file %s =======' "$core"
+      if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+        lldb -Q -o "bt all" -f "$(which java)" -c "${core}"
+      else
+        gdb -n -batch -ex "thread apply all bt" -ex "set pagination 0" "$(which java)" -c "${core}"
+      fi
+    done
+  fi
   exit ${RESULT}
 fi
 
