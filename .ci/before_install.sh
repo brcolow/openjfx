d109bb407eee5952acc6c7d25de6f1b462d0c070
Michael Ennen <mike.ennen@gmail.com>
2017-12-13 15:06:48 -0700

    Add support for Travis CI.

---
+++ .ci/before_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+  brew update
+  brew install findutils
+  brew install qt5
+  brew link qt5 --force
+  if [[ "${TRAVIS_JAVA_VERSION}" == 8 ]]; then
+    brew cask reinstall caskroom/versions/java8
+  elif [[ "${TRAVIS_JAVA_VERSION}" == 9 ]]; then
+    brew cask reinstall java
+  else
+      echo "TRAVIS_JAVA_VERSION environment variable not set!"
+  fi
+
+  brew outdated gradle || brew upgrade gradle
+  brew unlink python # fixes 'run_one_line' is not defined error in backtrace
+fi
---
+++ .ci/script.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+echo "which java: $(which java)"
+ulimit -c unlimited -S
+
+./gradlew build
+
+# Print core dumps when JVM crashes.
+RESULT=$?
+
+if [[ ${RESULT} -ne 0 ]]; then
+  ls "$TRAVIS_BUILD_DIR"/images
+  zip --verbose -r "$TRAVIS_BUILD_DIR"/images.zip "$TRAVIS_BUILD_DIR"/images/*
+
+  if [ ! -z ${PRINT_CRASH_LOGS+x} ]; then
+    if [[ "${TRAVIS_OS_NAME}" == osx ]]; then FIND="gfind"; else FIND="find"; fi
+    ${FIND} . -name "hs_err_pid*.log" -type f -printf '\n====== JVM CRASH LOG ======\n%p\n' -exec cat {} \;
+
+    CORES=''
+    if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+      CORES="$(find /cores -type f -regex '*core.[0-9]{4}' -print)"
+    else
+      CORES="$(find . -type f -regex '*core.[0-9]{6}' -print)"
+    fi
+
+    if [ -n "${CORES}" ]; then
+      for core in ${CORES}; do
+        printf '\n\n======= Core file %s =======\n' "$core"
+        if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+          lldb -Q -o "bt all" -f "$(which java)" -c "${core}"
+        else
+          gdb -n -batch -ex "thread apply all bt" -ex "set pagination 0" "$(which java)" -c "${core}"
+        fi
+      done
+    fi
+  fi
+  exit ${RESULT}
+  # exit 0 # Force success so images are uploaded.
+fi
+
+# vim :set ts=2 sw=2 sts=2 et:
